b465c253f2d1fab1bcfcbad8023859c43b9ae361
Spiro Metaxas <spirometaxas@gmail.com>
2025-08-30 16:28:15 -0400

    Add periodic-table-cli

---
+++ Formula/periodic-table-cli.rb
@@ -0,0 +1,26 @@
+class PeriodicTableCli < Formula
+  include Language::Python::Virtualenv
+
+  desc "An interactive Periodic Table of Elements app for the console!"
+  homepage "https://spirometaxas.com/projects/periodic-table-cli/"
+  url "https://files.pythonhosted.org/packages/f2/b3/de99f3bd627440d8fddfc3765ec3ca4fca816d6f237e0f0fefe95c065e8f/periodic_table_cli-2.1.2.tar.gz"
+  sha256 "1678af018f5131ec466cd6ecd05cf3330572cba65c9ea481e33c4303204ddef3"
+  license "MIT"
+
+  depends_on "python@3.12"
+
+  livecheck do
+    url :pypi
+    strategy :pypi
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    # Verify installation by printing the version info
+    output = shell_output("#{bin}/periodic-table-cli --version", 1)
+    assert_match "v2.1.2 (Python)", output
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # homebrew-tap
-Homebrew tap for periodic-table-cli
+
+## periodic-table-cli
+An interactive Periodic Table of Elements app for the console!
+
+![What periodic-table-cli prints to the console](https://raw.githubusercontent.com/spirometaxas/periodic-table-cli/main/img/animated.gif)
+
+- [Homepage](https://spirometaxas.com/projects/periodic-table-cli/)
+- [Repository](https://github.com/spirometaxas/periodic-table-cli-py)
+
+### Installation
+```bash
+$ brew tap spirometaxas/tap
+$ brew install periodic-table-cli
+```
+or in one line:
+```bash
+$ brew install spirometaxas/tap/periodic-table-cli
+```
+
+Note: Installs the Python version.
+
+## License
+- [MIT](https://github.com/spirometaxas/homebrew-tap/blob/main/LICENSE) &copy; [Spiro Metaxas](https://spirometaxas.com)
