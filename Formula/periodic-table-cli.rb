175ee6cc3ab94287c0466e94284bdd60d0104f9d
Spiro Metaxas <spirometaxas@gmail.com>
2025-08-30 20:51:14 -0400

    Update

--- Formula/periodic-table-cli.rb
+++ Formula/periodic-table-cli.rb
@@ -9,11 +9,6 @@ class PeriodicTableCli < Formula
 
   depends_on "python@3.12"
 
-  livecheck do
-    url :stable
-    strategy :pypi
-  end
-
   def install
     virtualenv_install_with_resources
   end
